be522d1242afda986581ac1ab4e04e046f239b73
Fit-For-Purpose Technologies <62441881+FFPTech@users.noreply.github.com>
2025-03-15 13:18:39 +0100

    Compile and gate

--- Basics/and_gate.v
+++ Basics/and_gate.v
@@ -1,16 +1,17 @@
+// iverilog -o and_gate_tb.out and_gate.v and_gate_tb.v
 // and_gate.v - Simple AND Gate Module
 // STEP 1: Module (circuit) definition
-module and_gate(A, B, Y);
-    input A, B;
-    output Y;
+module and_gate(X, Y, F);
+    input X, Y; // declare input variables
+    output F; // declare output variables
 
     // STEP 2: Module structure 
 
-    // a: AND logic (gates)
-    // and(Y, A, B); // port(outputs, inputs)
+    // a: AND logic (structure)
+    and(F, X, Y); // port(outputs, inputs)
 
     // b: AND logic (information)
-    assign Y = A & B; // C/C++ syntax
+    // assign F = X & Y; // C/C++ syntax
 
 // STEP 3: End module definition
 endmodule
--- Basics/and_gate_tb.v
+++ Basics/and_gate_tb.v
@@ -1,39 +1,40 @@
+// vvp and_gate_tb.out
 `timescale 1ns / 1ps
 
 // Define testing module
 module and_gate_tb; 
 
   // Declare testbench variables
-  reg A, B;      // Inputs
-  wire Y;        // Output
+  reg X, Y;      // Inputs
+  wire F;        // Output
 
   // Instantiate the AND gate module
   // uut = Unit-Under-Test
   and_gate uut (
-      .A(A),
-      .B(B),
-      .Y(Y)
+      .X(X),
+      .Y(Y),
+      .F(F)
   );  
 
   // Test sequence
   initial begin
     // Display header
     $display("Welcome to the ISIG-Goma e-Lab");
-    $display("A B | Y");
+    $display("X Y | F");
     $display("--------");
 
     // Apply test cases
-    A = 0; B = 0; #10; // set A=0, set B=0, wait 10ns
-    $display("%b %b | %b", A, B, Y); // C/C++ syntax
+    X = 0; Y = 0; #10; // set X=0, set Y=0, wait 10ns
+    $display("%b %b | %b", X, Y, F); // C/C++ syntax
 
-    A = 0; B = 1; #10; // set A=0, set B=1, wait 10ns
-    $display("%b %b | %b", A, B, Y);
+    X = 0; Y = 1; #10; // set X=0, set Y=1, wait 10ns
+    $display("%b %b | %b", X, Y, F);
 
-    A = 1; B = 0; #10;
-    $display("%b %b | %b", A, B, Y);
+    X = 1; Y = 0; #10;
+    $display("%b %b | %b", X, Y, F);
 
-    A = 1; B = 1; #10;
-    $display("%b %b | %b", A, B, Y);
+    X = 1; Y = 1; #10;
+    $display("%b %b | %b", X, Y, F);
 
     // End simulation
     $finish;
